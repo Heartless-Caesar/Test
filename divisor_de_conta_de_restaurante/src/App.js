385193a56e9cdc114e492ca43a291a50cacd4c12
caesar <joaovictorcsatu@gmail.com>
2022-08-24 10:23:31 -0300

    UPDATE COMMENTS

--- divisor_de_conta_de_restaurante/src/App.js
+++ divisor_de_conta_de_restaurante/src/App.js
@@ -9,6 +9,17 @@ function App() {
   let clientes = [];
   let produtos = [];
 
+  /*
+    TODO: ADICIONAR PRODUTOS EM SEU VETOR & BUSCAR PELO NOME DO CLIENTE A FIM DE GERAR SUA CONTA
+    E POSTERIORMENTE TRATAR COMO ADICIONAR NOVOS CLIENTES, FINALMENTE CALCULANDO O VALOR QUE CADA UM
+    TEM A PAGAR. 
+  */
+  const addProduct = ({ nome, produto, preco }) => {
+    produtos.push({ nome, produto, preco });
+    setProduto("");
+    setPreco(0);
+  };
+  const adicionarCliente = () => {};
   return (
     <div className="App">
       {/*ADICIONAR NOME CLIENTE*/}
@@ -30,17 +41,12 @@ function App() {
       <button
         type="button"
         onClick={() =>
-          clientes.push({ nome: nome, produto: produto, preco: preco })
+          addProduct({ nome: nome, produto: produto, preco: preco })
         }
       >
         Adicionar outro produto
       </button>
-      <button
-        type="button"
-        onClick={() =>
-          clientes.push({ nome: nome, produto: produto, preco: preco })
-        }
-      >
+      <button type="button" onClick={() => adicionarCliente(nome)}>
         Adicionar cliente
       </button>
     </div>
